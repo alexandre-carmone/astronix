535fbbf15b878cf7c4c688a259ca130cbf84f30a
Alexandre CARMONE <acarmone-prestataire@ina.fr>
2026-04-21 19:56:06 +0200

    fix headless

--- hosts/astronix/headless.nix
+++ hosts/astronix/headless.nix
@@ -25,10 +25,20 @@
   services.xserver = {
     enable = true;
 
+    monitorSection = ''
+      HorizSync   15.0 - 200.0
+      VertRefresh 15.0 - 200.0
+      Modeline "1920x1080" 173.00 1920 2048 2248 2576 1080 1083 1088 1120 -hsync +vsync
+      Modeline "2560x1440" 241.50 2560 2608 2640 2720 1440 1443 1448 1481 +hsync -vsync
+      Modeline "1680x1050" 146.25 1680 1784 1960 2240 1050 1053 1059 1089 -hsync +vsync
+      Option "PreferredMode" "1920x1080"
+    '';
+
     screenSection = ''
       DefaultDepth 24
       SubSection "Display"
         Depth 24
+        Modes "1920x1080" "2560x1440" "1680x1050"
         Virtual 2560 1440
       EndSubSection
     '';
