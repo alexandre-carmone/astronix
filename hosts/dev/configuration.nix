dffc86ad1b0131cefc6deae417590f1482900f21
Alexandre CARMONE <carmonealexandre@protonmail.ch>
2026-04-22 19:36:25 +0200

    add VPN

--- hosts/dev/configuration.nix
+++ hosts/dev/configuration.nix
@@ -8,20 +8,62 @@
 
   networking.hostName = "dev";
 
-  services.xserver = {
-    enable = true;
-    displayManager.gdm.enable = true;
-    desktopManager.gnome.enable = true;
-    xkb = {
-      layout = "us";
-      variant = "";
-    };
-  };
+  programs.firefox.enable = true;
+
+  services.displayManager.gdm.enable = true;
+  services.desktopManager.gnome.enable = true;
+
+  # To disable installing GNOME's suite of applications
+  # and only be left with GNOME shell.
+  services.gnome.core-apps.enable = false;
+  services.gnome.core-developer-tools.enable = false;
+  services.gnome.games.enable = false;
+  environment.gnome.excludePackages = with pkgs; [ gnome-tour gnome-user-docs ];
 
   services.printing.enable = true;
+  programs.dconf.profiles.user.databases = [
+    {
+      lockAll = true; # prevents overriding
+      settings = {
+        "org/gnome/desktop/interface" = {
+          accent-color = "blue";
+        };
+        "org/gnome/desktop/input-sources" = {
+          xkb-options = [ "nocaps:escape"];
+        };
+      };
+    }
+  ];
 
   environment.systemPackages = with pkgs; [
     gcc
+    git
     gnumake
+    gpclient
+    teams-for-linux
+    signal-desktop
+    btop
+    ffmpeg
+    vlc
+
+    gnomeExtensions.blur-my-shell
+    gnomeExtensions.just-perfection
+    adwaita-icon-theme
+    gnomeExtensions.arc-menu
   ];
+
+environment.etc."ssl/openssl-legacy.cnf".text = ''
+    openssl_conf = default_conf
+
+    [default_conf]
+    ssl_conf = ssl_sect
+
+    [ssl_sect]
+    system_default = ssl_default_sect
+
+    [ssl_default_sect]
+    Options = UnsafeLegacyRenegotiation
+  '';
+
+  environment.variables.OPENSSL_CONF = "/etc/ssl/openssl-legacy.cnf";
 }
