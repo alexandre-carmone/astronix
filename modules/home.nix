9b05f4b5cd47c056792968ffaa258f94d7649681
Alexandre CARMONE <acarmone-prestataire@ina.fr>
2026-08-01 07:43:42 +0200

    add zellij catppuccin

--- modules/home.nix
+++ modules/home.nix
@@ -1,11 +1,14 @@
 { inputs, ... }:
 
 # Home-manager wiring (used as a NixOS module) and the per-user home config:
-# Catppuccin Latte theming + Ghostty terminal.
+# Catppuccin Latte theming + Ghostty terminal + Zellij multiplexer.
 {
   home-manager.useUserPackages = true;
   home-manager.useGlobalPkgs = true;
   home-manager.extraSpecialArgs = { inherit inputs; };
+  # Back up (rather than clobber) any pre-existing dotfiles home-manager wants to
+  # manage, e.g. a zellij config.kdl generated on first run.
+  home-manager.backupFileExtension = "backup";
 
   home-manager.users.alexandre = { pkgs, inputs, ... }: {
     imports = [ inputs.catppuccin.homeModules.catppuccin ];
@@ -15,7 +18,11 @@
     catppuccin.ghostty.enable = true;
     catppuccin.gtk.icon.enable = true;
     catppuccin.cursors.enable = true;
+    catppuccin.zellij.enable = true;
     programs.ghostty = {
+      enable = true;
+    };
+    programs.zellij = {
       enable = true;
     };
     home.pointerCursor.gtk.enable = true;
